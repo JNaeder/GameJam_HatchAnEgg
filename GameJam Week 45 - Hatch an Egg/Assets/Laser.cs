47373a15148a73f55b12830fdabf1330554ef27d
JNaeder <j.naeder324@gmail.com>
2018-05-18 17:21:53 -0400

    Added Unity Folder

---
+++ GameJam Week 45 - Hatch an Egg/Assets/Laser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    public float speed;
+    ShipControl ship;
+    Egg egg;
+    public Quaternion rot;
+
+	// Use this for initialization
+	void Start () {
+        egg = FindObjectOfType<Egg>();
+        ship = FindObjectOfType<ShipControl>();
+        transform.localRotation = ship.transform.localRotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        transform.position = Vector3.MoveTowards(transform.position, egg.transform.position, speed * Time.deltaTime);
+
+
+
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);    
+        
+    }
+}
+    
---
+++ GameJam Week 45 - Hatch an Egg/Assets/ShipControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControl : MonoBehaviour {
+
+    public float speed;
+    public float fireRate;
+    public Transform egg;
+    public GameObject laser;
+
+    float firedTime, fireWaitTime;
+
+    // Use this for initialization
+    void Start () {
+        firedTime = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float h = -Input.GetAxis("Vertical");
+        transform.LookAt(egg);
+        transform.RotateAround(egg.position, Vector3.forward, speed * Time.deltaTime *h);
+
+        fireWaitTime = 1 / fireRate;
+
+
+        if (Input.GetMouseButton(0)) {
+            if (Time.time >= firedTime + fireWaitTime) {
+                
+                firedTime = Time.time;
+                GameObject firedLaser = Instantiate(laser, transform.position, Quaternion.identity);
+            }
+        }
+	}
+}
